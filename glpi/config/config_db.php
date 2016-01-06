0d834b064adfca81ff0f2dbc9f492e266ba1916e
JULIO8 <juliolucas57@gmail.com>
2016-01-06 11:21:57 -0500

    nuevo
    respaldo glpi

---
+++ glpi/config/config_db.php
@@ -0,0 +1,13 @@
+<?php
+ class DB extends DBmysql {
+                
+ var $dbhost = 'localhost:3307';
+                
+ var $dbuser 	= 'root';
+                
+ var $dbpassword= 'admin';
+                
+ var $dbdefault	= 'glpi';
+                
+ } 
+?>
